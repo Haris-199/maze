0e85bfe1c95d77f10aa0370fa72fcf0b8c2172a7
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-04-23 20:06:00 -0400

    Generate grid and cells

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
+const GRID = [];
+const w = 40;
+const h = w;
+let cellCount;
+
 function setup() {
     createCanvas(400, 400);
+    
+    cellCount = (width / w) * (height / 40);
+    
+    for (let i = 0; i < cellCount; i++) {
+        GRID[i] = new Cell(i);
+    }
 }
 
 function draw() {
