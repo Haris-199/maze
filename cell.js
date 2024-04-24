4a8a540ae9e24fae9efcd77d2044d3deb6f14cb6
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-04-24 03:06:01 -0400

    Implement maze generation with Kruskal's algorithm

--- cell.js
+++ cell.js
@@ -1,7 +1,7 @@
 class Cell {
 
     constructor(id) {
-        this.id = id;
+        this.setId = id;
         this.index = id;
         this.x = (this.index % cols) * W;
         this.y = floor(this.index / cols) * H;
@@ -9,6 +9,7 @@ class Cell {
     }
 
     show() {
+        textAlign(CENTER);
         stroke(200);
         if (this.sides[0])
             line(this.x, this.y, this.x + W, this.y); // top
@@ -18,6 +19,8 @@ class Cell {
             line(this.x + W, this.y + H, this.x, this.y + H); // bottom
         if (this.sides[3])
             line(this.x, this.y + H, this.x, this.y); // left
+        // fill(255);
+        // text(this.setId, this.x +W/2, this.y + H/2);
     }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 const GRID = [];
-const W = 40;
+let wallsSequence = [];
+const sets = [];
+const W = 20;
 const H = W;
 let rows, cols, cellCount;
 
@@ -12,12 +14,68 @@ function setup() {
 
     for (let i = 0; i < cellCount; i++) {
         GRID[i] = new Cell(i);
+        
+        sets[i] = new Set();
+        sets[i].add(GRID[i]);
+
+        let topId = i - cols;
+        let rightId = i + 1;
+        let bottomId = i + cols;
+        let leftId = i - 1;
+
+        if (i - cols >= 0)
+            wallsSequence.push([topId, i]);
+        if ((i + 1) % cols != 0)
+            wallsSequence.push([i, rightId]);
+        if (i + cols < cellCount)
+            wallsSequence.push([i, bottomId]);
+        if (i % cols != 0)
+            wallsSequence.push([leftId, i]);
     }
+
+    wallsSequence = wallsSequence.filter((t={}, a => !(t[a] = a in t)));
+    generateMaze();
 }
 
 function draw() {
     background(50);
     for (let i = 0; i < cellCount; i++) {
         GRID[i].show();
+    }
+}
+
+function generateMaze() {
+    let len = wallsSequence.length;
+
+    for (let i = 0; i < len; i++) {
+        let rand = Math.floor(Math.random() * len);
+        let temp = wallsSequence[i];
+        wallsSequence[i] = wallsSequence[rand];
+        wallsSequence[rand] = temp;
+    }
+
+    for (let i = 0; i < len; i++) {
+        let index1 = wallsSequence[i][0];
+        let index2 = wallsSequence[i][1];
+        let cell1 = GRID[index1];
+        let cell2 = GRID[index2];
+        let set1 = sets[cell1.setId];
+        let set2 = sets[cell2.setId];
+
+        if (set1 != set2) {
+            console.log("running");
+            let temp = cell2.setId;
+            set2.forEach( item => {item.setId = cell1.setId} );
+            sets[cell1.setId] = sets[cell1.setId].union(sets[temp]);
+            sets[temp].delete();
+
+            if (cell2.index - cell1.index == 1) {
+                cell1.sides[1] = false;
+                cell2.sides[3] = false;
+            } else if (cell2.index - cell1.index == cols) {
+                cell1.sides[2] = false;
+                cell2.sides[0] = false;
+            }
+        }
     }
 }
